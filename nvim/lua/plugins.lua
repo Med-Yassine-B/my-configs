bac5399c44018ff66e288d0db5c0acbf02cef65f
Med-Yassine-B <bouzidimouhamedyessin@gmail.com>
2026-09-04 22:32:14 +0100

    added friendly snippets

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -19,10 +19,20 @@ require("lazy").setup({
 
     -- Auto-completion and Snippets
     { "hrsh7th/nvim-cmp" },
+    {"saadparwaiz1/cmp_luasnip"},
+    {
+        "rafamadriz/friendly-snippets",
+        config=function ()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end
+    },
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/cmp-buffer" },
     { "hrsh7th/cmp-path" },
-    { "L3MON4D3/LuaSnip" },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies={"rafamadriz/friendly-snippets"}
+    },
     { "hrsh7th/cmp-cmdline" },
     { "hrsh7th/cmp-nvim-lua" },
 
